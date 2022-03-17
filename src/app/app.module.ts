6eb3ba94b1b91f9a28ae0b20d91e336c0c0c74fa
codepip2007 <pepcplace@gmail.com>
2022-03-17 09:15:27 +0800

    changed bundle size

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,41 +1,29 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 
-import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SharedModule } from './shared/shared.module';
-import { HomePageComponent } from './home-page/home-page.component';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuthModule } from '@angular/fire/compat/auth';
-import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
-import { ProjectsComponent } from './projects/projects.component';
-import { DiscordComponent } from './discord/discord.component';
-import { PrivacyPolicyComponent } from './privacy-policy/privacy-policy.component'
 
+// Components
+
+import { RouterModule } from '@angular/router';
+import { LayoutModule } from '@angular/cdk/layout';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomePageComponent,
-    ProjectsComponent,
-    DiscordComponent,
-    PrivacyPolicyComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule,
-    HttpClientModule,
-    AppRoutingModule,
-    BrowserAnimationsModule,
     NgbModule,
     SharedModule,
+    RouterModule,
     AngularFireModule.initializeApp(environment.firebase),
-    AngularFireAuthModule,
-    AngularFirestoreModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { ShellComponent } from './shell/shell.component';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatButtonModule } from '@angular/material/button';
 import { MatSidenavModule } from '@angular/material/sidenav';
@@ -14,12 +13,30 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+
 // Projects
 // import { BrowserModule } from '@angular/platform-browser';
 // import { HttpClientModule } from '@angular/common/http';
 
+// Components
+import { ShellComponent } from './shell/shell.component';
+import { HomePageComponent } from '../home-page/home-page.component';
+import { ProjectsComponent } from '../projects/projects.component';
+import { DiscordComponent } from '../discord/discord.component';
+import { PrivacyPolicyComponent } from '../privacy-policy/privacy-policy.component'
+
 const components = [
-  ShellComponent
+  ShellComponent,
+  HomePageComponent,
+  ProjectsComponent,
+  DiscordComponent,
+  PrivacyPolicyComponent,
 ];
 
 const modules = [
@@ -36,7 +53,12 @@ const modules = [
   MatCardModule,
   MatFormFieldModule,
   MatInputModule,
-  MatSnackBarModule
+  MatSnackBarModule,
+  AngularFireAuthModule,
+  AngularFirestoreModule,
+  BrowserModule,
+  HttpClientModule,
+  BrowserAnimationsModule
 ];
 
 @NgModule({
